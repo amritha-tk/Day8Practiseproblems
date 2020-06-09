d9bcc3c8cb0dc4fccf0d1217741aa2c0a1d53c09
amritha-tk <amrithatk95@gmail.com>
2020-06-10 00:26:45 +0530

    Modified

--- rolldice.sh
+++ rolldice.sh
@@ -1,51 +1,54 @@
-
-#!/bin/bash
+#!/bin/bash -x
 
 #To generate numbers from 1 to 6
 declare -A Dict
-
+declare -A Dict
 
 for(( i=0; i<$1; i++ ))
  do
   Dict[$i]=$(( RANDOM%6+1 ))
 done
 echo ${Dict[@]}
-x=0
+
+
+
 for (( i=0; i<${#Dict[@]}; i++ ))
  do
+   if [[ ! " ${a[@]} " =~ " ${Dict[$i]} " ]]; then
    count=1
-   ((x=x+1))
-  for (( j=($i+1) ; j<${#Dict[@]}; j++ ))
+   for (( j=($i+1) ; j<${#Dict[@]}; j++ ))
    do
-    if [[ ${Dict[$i]}  -eq  ${Dict[$j]} ]]
+     if [[ ${Dict[$i]}  -eq  ${Dict[$j]} ]]
      then
       ((count=count+1))
-          a[$x]=${Dict[$i]}
-
-          b[$x]=$count
-
+         a[$i]=${Dict[$i]}
+	 b[$i]=$count
 fi
 done
+fi
 done
 
 echo  "Duplicate  elements are " ${a[@]} "and their count is" ${b[@]}
+
 i=0
 max=0
-min=${b[$i]}
+min=${#Dict[@]}
+
 for y in ${b[@]}; do
-    if (( $y > $max ))
+    echo "i" $i
+    if (( $y>=$max ))
     then
      max=$y
-     #echo  "max value " ${a[$y]}
      max_value=${a[$i]}
-    if (( $y < $min ))
+fi
+    if (( $y<=$min ))
     then
     min=$y
     min_value=${a[$i]}
 fi
-fi
-    ((i=i+1)) 
+    ((i=i+1))
 done
 
 echo  " Max and Min value is " $max_value "," $min_value
 echo  "Max and Min count is " $max "," $min
+
